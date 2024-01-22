87c616ad168caff125d7e3d85675be0744e717c5
kendirliurunver <emrekendirli@urunver.com.tr>
2024-01-23 02:12:57 +0300

    first commit

---
+++ Acme.BookStore/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
+using AutoMapper;
+
+namespace Acme.BookStore;
+
+public class BookStoreApplicationAutoMapperProfile : Profile
+{
+    public BookStoreApplicationAutoMapperProfile()
+    {
+        CreateMap<Book, BookDto>();
+        CreateMap<Author, AuthorDto>();
+
+        CreateMap<CreateUpdateBookDto, Book>();
+        CreateMap<Author, AuthorLookupDto>();
+
+    }
+}
---
+++ Acme.BookStore/src/Acme.BookStore.Domain/Books/Book.cs
@@ -0,0 +1,25 @@
+﻿using Acme.BookStore.Authors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Books
+{
+    public class Book:AuditedAggregateRoot<Guid>
+    {
+        public Guid AuthorId { get; set; }
+
+        //public Author Author { get; set; }
+
+        public string Name { get; set; }
+
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+    }
+}
